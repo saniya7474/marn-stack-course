47f639e4f8228548853f756817e7cb78382a3891
saniya7474 <saniyaalase7757@gmail.com>
2025-06-09 14:52:57 +0530

    done with javascript

---
+++ 2 Month/javascript/1index.js
@@ -0,0 +1,23 @@
+//variables
+let name = "Saniya";
+const fathername = "Javed";
+var surname = "Alase";
+
+//Function
+function addNumbers(a,b){
+    console.log(a+b)
+}
+
+//show msg
+console.log("My name is Saniya Alase");
+
+//Arrays
+const firstArray=["Saniya","Sabiya","Neha"]
+
+//Objects
+const firstObject{
+    name : "saniya"
+}
+
+//string
+const firstString="Saniya Javed Alase"
---
+++ 2 Month/javascript/2index.js
@@ -0,0 +1,7 @@
+function writefullName(firstName, secondName, lastName) {
+
+    const fullName=firstName + secondName + lastName;
+
+    console.log(fullName);
+}
+writefullName("Saniya" ,"Javed", "Alase")
