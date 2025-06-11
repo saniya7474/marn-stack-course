ef619130c1b691831beb39bfe258413622268327
saniya7474 <saniyaalase7757@gmail.com>
2025-06-11 20:32:23 +0530

    done with ReactJs

---
+++ 2 Month/ReactJs/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Saniya Javed Alase.</h1>
+      <p>At.Post:- Ogalewadi,Tal:- Karad,Dist:- Satara</p>
+    </>
+  )
+}
+
+export default App
